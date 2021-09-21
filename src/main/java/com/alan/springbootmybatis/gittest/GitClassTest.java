6a3148f9e91fbf98ed2b72eeead6d20c8301ab93
whs@laptop <1594514210@qq.com>
2021-09-21 21:25:21 +0800

    新增git2test和修改GitClassTest

--- src/main/java/com/alan/springbootmybatis/gittest/GitClassTest.java
+++ src/main/java/com/alan/springbootmybatis/gittest/GitClassTest.java
@@ -2,4 +2,7 @@ package com.alan.springbootmybatis.gittest;
 
 public class GitClassTest {
     //add test
+    public static void main(String[] args) {
+
+    }
 }
---
+++ src/main/java/com/alan/springbootmybatis/gittest/git2test.java
@@ -0,0 +1,4 @@
+package com.alan.springbootmybatis.gittest;
+
+public class git2test {
+}
